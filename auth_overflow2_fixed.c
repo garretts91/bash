4cfbe634b24423d8c64e4a28d0673a2864dfc20c
squishybaker <grrtt.slmn@gmail.com>
2024-05-01 17:09:33 -0700

    general

--- auth_overflow2_fixed.c
+++ auth_overflow2_fixed.c
@@ -2,31 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Random function goes here
+// Returns the n-th element of fibonacci sequence
 
-int check_authentication(char *password) {
- char password_buffer[16];
-
- strcpy(password_buffer, password);
-
- if(strcmp(password_buffer, "michael") == 0)
- return 1;
- else if(strcmp(password_buffer, "tsikerdekis") == 0)
- return 1;
- else
- return 0;
+int fibonacci(int n)
+{
+    if (n < 2)
+        return n;
+    return fibo(n - 1) + fibo(n - 2);
 }
 
-int main(int argc, char *argv[]) {
- if(argc < 2) {
- printf("Usage: %s <password>\n", argv[0]);
- exit(0);
- }
- if(check_authentication(argv[1])) {
- printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
- printf(" Access Granted.\n");
- printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
- } else {
- printf("\nAccess Denied.\n");
- }
+int check_authentication(char *password)
+{
+    char password_buffer[16];
+
+    strncpy(password_buffer, password, sizeof(password_buffer) - 1);
+    password_buffer[sizeof(password_buffer) - 1] = '\0';
+
+    if (strcmp(password_buffer, "michael") == 0)
+        return 1;
+    else if (strcmp(password_buffer, "tsikerdekis") == 0)
+        return 1;
+    else
+        return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <password>\n", argv[0]);
+        exit(0);
+    }
+    if (check_authentication(argv[1]))
+    {
+        printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+        printf(" Access Granted.\n");
+        printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    }
+    else
+    {
+        printf("\nAccess Denied.\n");
+    }
 }
